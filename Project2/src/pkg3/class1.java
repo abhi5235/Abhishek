6772f6ac8d7a8208c62372553e08a91fd81282e1
Abhishek Pal <Abhishek Pal@ABHISHEK>
2022-05-04 15:06:47 +0530

    this is first time

---
+++ Project2/src/pkg3/class1.java
@@ -0,0 +1,16 @@
+package pkg3;
+
+public class class1 
+{
+ public static void main(String[] args) 
+ {
+	for (int i=1;i<=5;i++) 
+	{
+		for (int j=i;j<=5;j++)
+		{
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+}
+}
---
+++ Project2/src/pkg3/classStar.java
@@ -0,0 +1,20 @@
+package pkg3;
+
+public class classStar 
+{
+ public static void main(String[] args) 
+{
+ for(int i=1;i<=5;i++) 
+ {
+	 for(int s=i;s<=5;s++)
+	 {
+		 System.out.print(" ");
+	 }
+	 for(int j=1*1-i;j<i;j++)
+	 {
+		 System.out.print("*");
+	 }
+	 System.out.println();
+ }
+}
+}
